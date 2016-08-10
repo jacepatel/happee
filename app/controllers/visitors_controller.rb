8fed1677d6678284aea62f194891f4a7dc64918b
Jace Patel <jace.patel@gmail.com>
2016-08-10 14:01:49 +1000

    Add gitignore

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,5 @@
-class VisitorsController < SecureController
+class VisitorsController < ApplicationController
   # The index for all visitors - the controller for the home page
   def index
-
   end
 end
